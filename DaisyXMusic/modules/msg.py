5f114518ce7b0c83ff4d2bcb6c954a5073081abe
Aquila <78516399+Aquila-14@users.noreply.github.com>
2021-06-12 17:03:22 +0530

    Update msg.py

--- DaisyXMusic/modules/msg.py
+++ DaisyXMusic/modules/msg.py
@@ -17,15 +17,15 @@
 import os
 from DaisyXMusic.config import SOURCE_CODE,ASSISTANT_NAME,PROJECT_NAME,SUPPORT_GROUP,UPDATES_CHANNEL
 class Messages():
-      START_MSG = "**Hello 👋 [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
+      START_MSG = "**Hello [{}](tg://user?id={})!**\n\n🤖 I am an advanced bot created for playing music in the voice chats of Telegram Groups & Channels.\n\n✅ Send me /help for more info."
       HELP_MSG = [
         ".",
 f"""
-**Hey 👋 Welcome back to {PROJECT_NAME}
+**Hey Welcome back to {PROJECT_NAME}
 
-⚪️ {PROJECT_NAME} can play music in your group's voice chat as well as channel voice chats
+༆ {PROJECT_NAME} can play music in your group's voice chat as well as channel voice chats
 
-⚪️ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions**
+༆ Assistant name >> @{ASSISTANT_NAME}\n\nClick next for instructions**
 """,
 
 f"""
@@ -43,7 +43,7 @@ f"""
 
 **Commands**
 
-**=>> Song Playing 🎧**
+**=>> Song Playing 🎵**
 
 - /play: Play the requestd song
 - /play [yt url] : Play the given yt url
@@ -66,9 +66,9 @@ f"""
 """,
         
 f"""
-**=>> Channel Music Play 🛠**
+**=>> Channel Music Play 🎼**
 
-⚪️ For linked group admins only:
+༆ For linked group admins only:
 
 - /cplay [song name] - play song you requested
 - /cdplay [song name] - play song you requested via deezer
@@ -84,7 +84,7 @@ f"""
 
 channel is also can be used instead of c ( /cplay = /channelplay )
 
-⚪️ If you donlt like to play in linked group:
+༆ If you donlt like to play in linked group:
 
 1) Get your channel ID.
 2) Create a group with tittle: Channel Music: your_channel_id
@@ -94,13 +94,13 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 """,
 
 f"""
-**=>> More tools 🧑‍🔧**
+**=>> More tools 🔍**
 
 - /musicplayer [on/off]: Enable/Disable Music player
 - /admincache: Updates admin info of your group. Try if bot isn't recognize admin
 - /userbotjoin: Invite @{ASSISTANT_NAME} Userbot to your chat
 
-**=>> Commands for Sudo Users ⚔️**
+**=>> Commands for Sudo Users ✪**
 
  - /userbotleaveall - remove assistant from all chats
  - /gcast <reply to message> - globally brodcast replied message to all chats
