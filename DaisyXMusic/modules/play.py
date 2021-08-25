a2dda1a71b7c3cd43c24aaaf72ec6507f25e5214
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-08-25 20:09:07 +0530

    Update play.py

--- DaisyXMusic/modules/play.py
+++ DaisyXMusic/modules/play.py
@@ -75,3 +75,237 @@ def transcode(filename):
     os.remove(filename)
 
 
+
+# Convert seconds to mm:ss
+def convert_seconds(seconds):
+    seconds = seconds % (24 * 3600)
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02d:%02d" % (minutes, seconds)
+
+
+# Convert hh:mm:ss to seconds
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+# Change image size
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
+
+
+async def generate_cover(requested_by, title, views, duration, thumbnail):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open("background.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+
+    image1 = Image.open("./background.png")
+    image2 = Image.open("./etc/foreground.png")
+    image3 = changeImageSize(1280, 720, image1)
+    image4 = changeImageSize(1280, 720, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save("temp.png")
+    img = Image.open("temp.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("etc/font.otf", 32)
+    draw.text((205, 550), f"Title: {title}", (51, 215, 255), font=font)
+    draw.text((205, 590), f"Duration: {duration}", (255, 255, 255), font=font)
+    draw.text((205, 630), f"Views: {views}", (255, 255, 255), font=font)
+    draw.text(
+        (205, 670),
+        f"Added By: {requested_by}",
+        (255, 255, 255),
+        font=font,
+    )
+    img.save("final.png")
+    os.remove("temp.png")
+    os.remove("background.png")
+
+
+@Client.on_message(filters.command("playlist") & filters.group & ~filters.edited)
+async def playlist(client, message):
+    global que
+    if message.chat.id in DISABLED_GROUPS:
+        return    
+    queue = que.get(message.chat.id)
+    if not queue:
+        await message.reply_text("Player is idle")
+    temp = []
+    for t in queue:
+        temp.append(t)
+    now_playing = temp[0][0]
+    by = temp[0][1].mention(style="md")
+    msg = "**Now Playing** in {}".format(message.chat.title)
+    msg += "\n- " + now_playing
+    msg += "\n- Req by " + by
+    temp.pop(0)
+    if temp:
+        msg += "\n\n"
+        msg += "**Queue**"
+        for song in temp:
+            name = song[0]
+            usr = song[1].mention(style="md")
+            msg += f"\n- {name}"
+            msg += f"\n- Req by {usr}\n"
+    await message.reply_text(msg)
+
+
+# ============================= Settings =========================================
+
+
+def updated_stats(chat, queue, vol=100):
+    if chat.id in callsmusic.pytgcalls.active_calls:
+        # if chat.id in active_chats:
+        stats = "Settings of **{}**".format(chat.title)
+        if len(que) > 0:
+            stats += "\n\n"
+            stats += "Volume : {}%\n".format(vol)
+            stats += "Songs in queue : `{}`\n".format(len(que))
+            stats += "Now Playing : **{}**\n".format(queue[0][0])
+            stats += "Requested by : {}".format(queue[0][1].mention)
+    else:
+        stats = None
+    return stats
+
+
+def r_ply(type_):
+    if type_ == "play":
+        pass
+    else:
+        pass
+    mar = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("⏹", "leave"),
+                InlineKeyboardButton("⏸", "puse"),
+                InlineKeyboardButton("▶️", "resume"),
+                InlineKeyboardButton("⏭", "skip"),
+            ],
+            [
+                InlineKeyboardButton("Playlist 📖", "playlist"),
+            ],
+            [InlineKeyboardButton("❌ Close", "cls")],
+        ]
+    )
+    return mar
+
+
+@Client.on_message(filters.command("current") & filters.group & ~filters.edited)
+async def ee(client, message):
+    if message.chat.id in DISABLED_GROUPS:
+        return
+    queue = que.get(message.chat.id)
+    stats = updated_stats(message.chat, queue)
+    if stats:
+        await message.reply(stats)
+    else:
+        await message.reply("No VC instances running in this chat")
+
+
+@Client.on_message(filters.command("player") & filters.group & ~filters.edited)
+@authorized_users_only
+async def settings(client, message):
+    if message.chat.id in DISABLED_GROUPS:
+        await message.reply("Music Player is Disabled")
+        return    
+    playing = None
+    chat_id = get_chat_id(message.chat)
+    if chat_id in callsmusic.pytgcalls.active_calls:
+        playing = True
+    queue = que.get(chat_id)
+    stats = updated_stats(message.chat, queue)
+    if stats:
+        if playing:
+            await message.reply(stats, reply_markup=r_ply("pause"))
+
+        else:
+            await message.reply(stats, reply_markup=r_ply("play"))
+    else:
+        await message.reply("No VC instances running in this chat")
+
+
+@Client.on_message(
+    filters.command("musicplayer") & ~filters.edited & ~filters.bot & ~filters.private
+)
+@authorized_users_only
+async def hfmm(_, message):
+    global DISABLED_GROUPS
+    try:
+        user_id = message.from_user.id
+    except:
+        return
+    if len(message.command) != 2:
+        await message.reply_text(
+            "I only recognize `/musicplayer on` and /musicplayer `off only`"
+        )
+        return
+    status = message.text.split(None, 1)[1]
+    message.chat.id
+    if status == "ON" or status == "on" or status == "On":
+        lel = await message.reply("`Processing...`")
+        if not message.chat.id in DISABLED_GROUPS:
+            await lel.edit("Music Player Already Activated In This Chat")
+            return
+        DISABLED_GROUPS.remove(message.chat.id)
+        await lel.edit(
+            f"Music Player Successfully Enabled For Users In The Chat {message.chat.id}"
+        )
+
+    elif status == "OFF" or status == "off" or status == "Off":
+        lel = await message.reply("`Processing...`")
+
+        if message.chat.id in DISABLED_GROUPS:
+            await lel.edit("Music Player Already turned off In This Chat")
+            return
+        DISABLED_GROUPS.append(message.chat.id)
+        await lel.edit(
+            f"Music Player Successfully Deactivated For Users In The Chat {message.chat.id}"
+        )
+    else:
+        await message.reply_text(
+            "I only recognize `/musicplayer on` and /musicplayer `off only`"
+        )    
+
+
+@Client.on_callback_query(filters.regex(pattern=r"^(playlist)$"))
+async def p_cb(b, cb):
+    global que
+    que.get(cb.message.chat.id)
+    type_ = cb.matches[0].group(1)
+    cb.message.chat.id
+    cb.message.chat
+    cb.message.reply_markup.inline_keyboard[1][0].callback_data
+    if type_ == "playlist":
+        queue = que.get(cb.message.chat.id)
+        if not queue:
+            await cb.message.edit("Player is idle")
+        temp = []
+        for t in queue:
+            temp.append(t)
+        now_playing = temp[0][0]
+        by = temp[0][1].mention(style="md")
+        msg = "**Now Playing** in {}".format(cb.message.chat.title)
+        msg += "\n- " + now_playing
+        msg += "\n- Req by " + by
+        temp.pop(0)
+        if temp:
+            msg += "\n\n"
+            msg += "**Queue**"
+            for song in temp:
+                name = song[0]
+                usr = song[1].mention(style="md")
+                msg += f"\n- {name}"
+                msg += f"\n- Req by {usr}\n"
+        await cb.message.edit(msg)
+
